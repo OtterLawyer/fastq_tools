fd4702b5b05b7a16c412adef43ef6d42acb6ade7
OtterLawyer <130027058+OtterLawyer@users.noreply.github.com>
2023-10-08 16:33:42 +0900

    Change import to import as

---
+++ bioinf_tools.py
@@ -0,0 +1,5 @@
+from packages.dna_tools import run_dna_rna_tools as na_tools
+from packages.protein_tools import protein_tools as p_tools
+from packages.fastq_tools import fastq_tools as fq_tools
+from typing import Union
+
